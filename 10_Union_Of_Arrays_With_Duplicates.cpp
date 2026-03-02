e7b2c16386a87acf12da10b2ed7cbb57c9409a1d
Jinansh28 <jinansh2807@gmail.com>
2026-03-02 10:17:26 +0530

    Day 10

---
+++ 10_Union_Of_Arrays_With_Duplicates.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> findUnion(vector<int>& a, vector<int>& b) {
+        unordered_set<int> s;
+        
+        for(int i = 0; i < a.size() ; i++) s.insert(a[i]);
+        for(int i = 0; i < b.size() ; i++) s.insert(b[i]);
+        
+        vector<int> ans;
+        for(int i : s) ans.push_back(i);
+        return ans;
+    }
+};
